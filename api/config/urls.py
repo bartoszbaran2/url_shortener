a64eaa5d9d6890ae207304fc0ebb68e53518c8a2
Bartosz Baran <bartosz.baran192@gmail.com>
2023-05-11 19:43:24 +0200

    secure admin url

--- api/config/urls.py
+++ api/config/urls.py
@@ -19,7 +19,7 @@ from django.urls import path, include
 from drf_spectacular.views import SpectacularAPIView, SpectacularSwaggerView
 
 urlpatterns = [
-    path("admin/", admin.site.urls),
+    path("5ecret4dm1n/", admin.site.urls),
     path("", include("shortener.urls")),
     path("api/schema/", SpectacularAPIView.as_view(), name="schema"),
     path("api/schema/swagger-ui/", SpectacularSwaggerView.as_view(url_name="schema"), name="swagger-ui"),
